f4b4b49644030ce560cb978ec004d2dabe99b5d0
wanmmmme <metineechaturaphat@gmail.com>
2024-03-11 19:26:25 +0700

    Chang lable to thai-lang

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,8 +9,8 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="=ชื่อ-นามสกุล *"
+        hint="ชื่อจริง นามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
